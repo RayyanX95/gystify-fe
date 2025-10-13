63aaaf686e327112874d13edc7e1a6323c816417
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2025-10-13 21:06:36 +0300

    refactor: remove SummariesHistory and SummaryDetailsLoader components from the dashboard

--- src/app/(dashboard)/_components/index.ts
+++ src/app/(dashboard)/_components/index.ts
@@ -1,7 +1,5 @@
 export { FeatureSectionLoader } from './FeatureSectionLoader';
-export { SummaryDetailsLoader } from './SummaryDetailsLoader';
 export { MetricsCards } from './MetricsCards';
-export { SummariesHistory } from './SummariesHistory';
 export { CategoriesTabs } from './CategoriesTabs';
 export { StatsCard } from './StatsCard';
 export { SnapshotOverview } from './SnapshotOverview';
